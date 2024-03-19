6bfeec9a15a0eed8cbd3251f52a5485da13d609b
Abubakar1010 <abubakar850772@gmail.com>
2024-03-19 17:17:28 +0600

    project initialized with react and tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className=' text-2xl font-medium bg-purple-500 border-pink-400 border-2 p-3 w-96 text-center text-white my-12 mx-auto rounded-lg hover:scale-90 duration-1000'>React Router Crash Course</h1>
+    </>
+  )
+}
+
+export default App
